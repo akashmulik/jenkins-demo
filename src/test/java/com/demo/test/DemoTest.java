b271a30a64e5ed752efd1733a57dfb6ca2af80e8
Akash Mulik <akashmulik@users.noreply.github.com>
2018-10-23 00:38:23 +0530

    creating code

---
+++ src/test/java/com/demo/test/DemoTest.java
@@ -0,0 +1,14 @@
+package com.demo.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DemoTest {
+
+	@Test
+	public void demoTest() {
+		
+		assertEquals(true, true);
+	}
+}
